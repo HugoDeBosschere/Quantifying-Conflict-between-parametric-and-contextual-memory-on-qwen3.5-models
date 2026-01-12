f8808c8bb15821fdf22bb3c5b442cb49fa5dd87c
maximilien <maximilien.lyonnais@student-cs.fr>
2026-01-12 23:21:37 +0100

    dataset with exec_context containng the WrapNumpyDataset was added ---> to be automised later

--- data/data_modif.py
+++ data/data_modif.py
@@ -1,6 +1,7 @@
 import re
+import json
 
-def modify_numpy_in_context(file_content, new_import_statement):
+def modify_numpy(file_content, new_import_statement):
     # EXPLICATION DU PATTERN :
     # 1. (exec_context\s*=\s*r?""")  -> Groupe 1 : Capture le nom de la variable et l'ouverture des guillemets (r""" ou """)
     # 2. (.*?)                       -> Groupe 2 : Capture tout le texte AVANT l'import (non-gourmand)
@@ -29,4 +30,31 @@ def modify_numpy_in_context(file_content, new_import_statement):
     else:
         print("Aucune occurrence trouvée dans exec_context.")
         return file_content
+
+
+
+if __name__ == "__main__" :
+
+
+    lib_to_try = "import WrapRotatedNumpy as np"
+    input_path = "/usr/users/sdim/sdim_25/memory_code_eval/data/ds1000_npy.jsonl"
+    output_path = "/usr/users/sdim/sdim_25/memory_code_eval/data/ds1000_Wnpy.jsonl"
+    with open(input_path, 'r', encoding='utf-8') as f_in, \
+         open(output_path, 'a', encoding='utf-8') as f_out:
+        for line in f_in :
+            if not line.strip():
+                continue
+            task = json.loads(line)
+
+            string_to_mod = task["code_context"]
+
+            new_context = modify_numpy(string_to_mod, lib_to_try)
+
+            # write the new code context
+            task["code_context"] = new_context
+            json_record = json.dumps(task)
+            f_out.write(json_record + "\n")
+            f_out.flush()
+            
+            
 
