e2f659634340980726a6e5e5e83977813a120c8c
hugo <hugo.debosschere@student-cs.fr>
2026-01-28 22:42:59 +0100

    ajout de generate_minimal_documentation qui permet de générer une documentation avec seulement le nom des fonctions et leur signature quand elle existe

--- src/documentation/underscore_numpy_documentation.py
+++ src/documentation/underscore_numpy_documentation.py
@@ -31,7 +31,16 @@ def add_underscore_signature(docstring):
     """
     adds an underscore to the signature of a function 
     """
-    new_signature = re.sub(r"^(\w+)\(",r"\1_(", docstring)
+    first_pattern = re.search(".*?(?=\n)", docstring)
+    
+    if first_pattern:
+        pattern = re.search(r"(\w+)\(.*", first_pattern.group(0))
+        if pattern:
+            new_signature = re.sub(r"^(\w+)\(",r"\1_(", pattern.group(0))
+        else:
+            new_signature = None 
+    else:
+        new_signature = None
     #print(f"Ceci est la nouvelle signature :{new_signature}")
     return new_signature
 
@@ -50,7 +59,6 @@ def add_underscore_see_also(docstring):
     new_see_also = re.sub(r"See Also\n--------\n(\w+),",r"\1_",pattern,docstring)
     print(f"New see also:{new_see_also}")
     return new_see_also
-
 
 def corrupt_doc(doc_text,shorthand):
     if not doc_text: return ""
@@ -114,7 +122,7 @@ def generate_full_docs(list_module,list_shorthand, output_file):
                         raw_doc = obj.__doc__
                         
                 
-                        new_doc = test_doc
+                        new_doc = raw_doc
                         
                         if new_doc:
                             f.write(f"FUNCTION: {full_name}\n")
@@ -122,9 +130,9 @@ def generate_full_docs(list_module,list_shorthand, output_file):
                             for shorthand in list_shorthand:
                                 # Write to file in a clean format
                                 new_doc = corrupt_doc(new_doc,shorthand)
-                                break
+                        
                             new_doc = add_underscore_signature(new_doc)
-                            new_doc = add_underscore_see_also(new_doc)
+                            #new_doc = add_underscore_see_also(new_doc)
                             f.write(new_doc + "\n")
                             f.write("\n" + "#"*40 + "\n\n")
                             
@@ -133,10 +141,155 @@ def generate_full_docs(list_module,list_shorthand, output_file):
                         # Only crawl submodules that belong to numpy (avoid crawling 'sys' or 'os')
                         if hasattr(obj, '__name__') and 'numpy' in obj.__name__:
                             stack.append((obj, f"{prefix}.{name}"))
-            break
+            
     print(f"Documentation generated in {output_file}")
     print(f"Total unique functions documented: {len(seen_functions)}")
 
-# --- 3. Run it ---
+
+def generate_ultra_minimal_docs(list_module,output_file):
+    """
+    Generate a doc with only the function names and nothing else
+    """
+     # Set to keep track of functions we've already documented (to avoid duplicates)
+    seen_functions = set()
+    with open(output_file, 'w', encoding='utf-8') as f:
+        for base_module in tqdm.tqdm(list_module):
+            f.write(f"Reference Documentation for {base_module.__name__} \n")
+            f.write("="*60 + "\n\n")
+
+            # We use a stack to crawl submodules (starting with numpy itself)
+            # Format: (module_object, name_prefix)
+            stack = [(base_module, base_module.__name__)]
+            
+            # Limit recursion depth to avoid crawling the entire python world
+            visited_modules = set()
+
+            while stack:
+                current_mod, prefix = stack.pop()
+                
+                if current_mod in visited_modules:
+                    continue
+                visited_modules.add(current_mod)
+
+                # Get everything inside this module
+                for name in dir(current_mod):
+                    # Skip private internal stuff
+                    if name.startswith("_"): continue
+                    
+                    try:
+                        obj = getattr(current_mod, name)
+                    except:
+                        continue
+
+                    # A. IF IT IS A FUNCTION: Document it
+                    if inspect.isfunction(obj) or isinstance(obj, numpy.ufunc):
+                        # Check if we already documented this exact function object
+                        # (NumPy exposes the same function in multiple places)
+                        if obj in seen_functions:
+                            continue
+                        seen_functions.add(obj)
+                        
+                        # Create the full name (e.g., numpy.linalg.norm)
+                        full_name = f"{prefix}.{name}_"
+                        
+
+
+                        # Get docstring
+                        raw_doc = obj.__doc__
+                        
+                
+                        new_doc = test_doc
+                        
+                    
+                        f.write(f"FUNCTION: {full_name}\n")
+                        f.write("-" * (10 + len(full_name)) + "\n")
+                                                                                                              
+                    # B. IF IT IS A SUB-MODULE: Add to stack to crawl later
+                    elif isinstance(obj, types.ModuleType):
+                        # Only crawl submodules that belong to numpy (avoid crawling 'sys' or 'os')
+                        if hasattr(obj, '__name__') and 'numpy' in obj.__name__:
+                            stack.append((obj, f"{prefix}.{name}"))
+            
+    print(f"Documentation generated in {output_file}")
+    print(f"Total unique functions documented: {len(seen_functions)}")
+
+
+def generate_minimal_docs(list_module,output_file):
+    """
+    Generate a doc with the function names and the signature, when it exists
+    """
+     # Set to keep track of functions we've already documented (to avoid duplicates)
+    seen_functions = set()
+    with open(output_file, 'w', encoding='utf-8') as f:
+        for base_module in tqdm.tqdm(list_module):
+            f.write(f"Reference Documentation for {base_module.__name__} \n")
+            f.write("="*60 + "\n\n")
+
+            # We use a stack to crawl submodules (starting with numpy itself)
+            # Format: (module_object, name_prefix)
+            stack = [(base_module, base_module.__name__)]
+            
+            # Limit recursion depth to avoid crawling the entire python world
+            visited_modules = set()
+
+            while stack:
+                current_mod, prefix = stack.pop()
+                
+                if current_mod in visited_modules:
+                    continue
+                visited_modules.add(current_mod)
+
+                # Get everything inside this module
+                for name in dir(current_mod):
+                    # Skip private internal stuff
+                    if name.startswith("_"): continue
+                    
+                    try:
+                        obj = getattr(current_mod, name)
+                    except:
+                        continue
+
+                    # A. IF IT IS A FUNCTION: Document it
+                    if inspect.isfunction(obj) or isinstance(obj, numpy.ufunc):
+                        # Check if we already documented this exact function object
+                        # (NumPy exposes the same function in multiple places)
+                        if obj in seen_functions:
+                            continue
+                        seen_functions.add(obj)
+                        
+                        # Create the full name (e.g., numpy.linalg.norm)
+                        full_name = f"{prefix}.{name}_"
+                        
+
+
+                        # Get docstring
+                        raw_doc = obj.__doc__
+                        
+
+                        new_doc = raw_doc
+                        if new_doc:
+                            modif = add_underscore_signature(new_doc)
+                            if modif:
+                                print(f"modif dans la boucle : {modif}")
+                                new_doc = modif
+                            else:
+                                new_doc = ""
+                            f.write(f"FUNCTION: {full_name}\n")
+                            f.write("-" * (10 + len(full_name)) + "\n")
+                            f.write(new_doc + "\n")
+                            f.write("#"*40 + "\n")                                      
+                    # B. IF IT IS A SUB-MODULE: Add to stack to crawl later
+                    elif isinstance(obj, types.ModuleType):
+                        # Only crawl submodules that belong to numpy (avoid crawling 'sys' or 'os')
+                        if hasattr(obj, '__name__') and 'numpy' in obj.__name__:
+                            stack.append((obj, f"{prefix}.{name}"))
+            
+    print(f"Documentation generated in {output_file}")
+    print(f"Total unique functions documented: {len(seen_functions)}")
+
+
+
 if __name__ == "__main__":
-    generate_full_docs([numpy,scipy,pandas,matplotlib.pyplot],["np","scipy","pd","numpy","pandas","maplotlib","plt"], "src/documentation/numpy_manual_underscore.txt")
+    #generate_full_docs([numpy,scipy,pandas,matplotlib.pyplot],["np","numpy"], "numpy_manual_underscore.txt")
+    #generate_ultra_minimal_docs([numpy],output_file="utlra_minimal_numpy.txt")
+    generate_minimal_docs([numpy],output_file="minimal_numpy.txt")
